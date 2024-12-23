c58df46f71dc2d62aec54fdeb2903060ef2cd213
Yusup <yusup@lsgrep.com>
2024-12-23 21:13:15 +0800

    use device addr

--- gateway/gateway_darwin.go
+++ gateway/gateway_darwin.go
@@ -10,72 +10,61 @@ import (
 )
 
 func detectOutgoingIface() (addr netip.Addr, ifindex uint32, ok bool) {
-	rib, err := route.FetchRIB(syscall.AF_INET, syscall.NET_RT_DUMP, 0)
+	// Try IPv4 first
+	rib, err := route.FetchRIB(syscall.AF_INET, syscall.NET_RT_IFLIST, 0)
 	if err != nil {
 		return netip.Addr{}, 0, false
 	}
 
-	msgs, err := route.ParseRIB(syscall.NET_RT_DUMP, rib)
+	msgs, err := route.ParseRIB(syscall.NET_RT_IFLIST, rib)
 	if err != nil {
 		return netip.Addr{}, 0, false
 	}
 
 	for _, m := range msgs {
-		rm, ok := m.(*route.RouteMessage)
-		if !ok {
-			continue
-		}
-
-		// Look for default route (destination 0.0.0.0/0)
-		if len(rm.Addrs) <= syscall.RTAX_DST || len(rm.Addrs) <= syscall.RTAX_GATEWAY {
-			continue
-		}
-
-		dst := rm.Addrs[syscall.RTAX_DST]
-		gw := rm.Addrs[syscall.RTAX_GATEWAY]
-
-		// Check if this is a default route (0.0.0.0/0)
-		if dst == nil {
-			continue
-		}
-
-		switch d := dst.(type) {
-		case *route.Inet4Addr:
-			// Check if this is the default route (0.0.0.0)
-			if d.IP != [4]byte{0, 0, 0, 0} {
+		switch msg := m.(type) {
+		case *route.InterfaceMessage:
+			if msg.Flags&syscall.IFF_UP == 0 || msg.Flags&syscall.IFF_LOOPBACK != 0 {
 				continue
 			}
-
-			// Found default route, get gateway
-			if gw == nil {
-				continue
-			}
-
-			switch g := gw.(type) {
-			case *route.Inet4Addr:
-				ip := netip.AddrFrom4([4]byte(g.IP))
-				if !ip.IsLoopback() {
-					return ip, uint32(rm.Index), true
+			for _, a := range msg.Addrs {
+				switch addr := a.(type) {
+				case *route.Inet4Addr:
+					ip := netip.AddrFrom4(addr.IP)
+					if !ip.IsLoopback() && !ip.IsUnspecified() {
+						return ip, uint32(msg.Index), true
+					}
 				}
 			}
-		case *route.Inet6Addr:
-			// Check if this is the default route (::/0)
-			if !isZeroIPv6(d.IP) {
+		}
+	}
+
+	// Try IPv6 if IPv4 failed
+	rib, err = route.FetchRIB(syscall.AF_INET6, syscall.NET_RT_IFLIST, 0)
+	if err != nil {
+		return netip.Addr{}, 0, false
+	}
+
+	msgs, err = route.ParseRIB(syscall.NET_RT_IFLIST, rib)
+	if err != nil {
+		return netip.Addr{}, 0, false
+	}
+
+	for _, m := range msgs {
+		switch msg := m.(type) {
+		case *route.InterfaceMessage:
+			if msg.Flags&syscall.IFF_UP == 0 || msg.Flags&syscall.IFF_LOOPBACK != 0 {
 				continue
 			}
-
-			// Found default route, get gateway
-			if gw == nil {
-				continue
-			}
-
-			switch g := gw.(type) {
-			case *route.Inet6Addr:
-				var ipBytes [16]byte
-				copy(ipBytes[:], g.IP[:])
-				ip := netip.AddrFrom16(ipBytes)
-				if !ip.IsLoopback() {
-					return ip, uint32(rm.Index), true
+			for _, a := range msg.Addrs {
+				switch addr := a.(type) {
+				case *route.Inet6Addr:
+					var ipBytes [16]byte
+					copy(ipBytes[:], addr.IP[:])
+					ip := netip.AddrFrom16(ipBytes)
+					if !ip.IsLoopback() && !ip.IsUnspecified() && !ip.IsLinkLocalUnicast() {
+						return ip, uint32(msg.Index), true
+					}
 				}
 			}
 		}
